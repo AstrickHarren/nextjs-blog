c7d5abf92d434ce16892bd3037ab6ae9bae6b60d
Astrick <astrick.harren.at.work@gmail.com>
2023-10-02 10:13:57 -0400

    init: nextJS blog proj

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
-import { allPosts } from "@/.contentlayer/generated"
-import Link from "next/link"
+import { allPosts } from "@/.contentlayer/generated";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="prose dark:prose-invert">
       {allPosts.map((post) => (
-        <article key={post._id}>
-          <Link href={post.slug}>
-            <h2>{post.title}</h2>
-          </Link>
-          {post.description && <p>{post.description}</p>}
-        </article>
+        <Link href={post.slug} style={{ textDecoration: "none" }}>
+          <div className="border-2 border-stone-100 rounded m-4 px-10 shadow backdrop-blur-md">
+            <article key={post._id}>
+              <h2>{post.title}</h2>
+              {post.description && (
+                <p className="text-secondary">{post.description}</p>
+              )}
+            </article>
+          </div>
+        </Link>
       ))}
     </div>
-  )
+  );
 }
